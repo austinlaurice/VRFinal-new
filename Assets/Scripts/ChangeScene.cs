9d3ff17d39cc47f64cee34cb0025780670e10eea
tenyoku8478 <sh961014@gmail.com>
2017-06-19 08:52:03 +0800

    floor 3

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+    static private HashSet<string> loadedScenes = new HashSet<string>();
+    public string scene;
+    public Vector3 position;
+	// Use this for initialization
+	void Start () {
+        loadedScenes.Add(Application.loadedLevelName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "FPSController")
+        {
+            if (!loadedScenes.Contains(scene))
+            {
+                Application.LoadLevelAdditive(scene);
+                loadedScenes.Add(scene);
+            }
+            GameObject.Find("FPSController").transform.position = position;
+            //SceneManager.LoadScene(scene);
+        }
+    }
+}
