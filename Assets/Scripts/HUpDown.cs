ac5f4dc8553147b3e695a7174f89db15ace490b8
tenyoku8478 <sh961014@gmail.com>
2017-06-19 11:03:04 +0800

    update letter

---
+++ Assets/Scripts/HUpDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUpDown : MonoBehaviour {
+    public Camera camera;
+    private ZoomInDisplay display;
+    private ChangeScene up, down;
+	// Use this for initialization
+	void Start () {
+        display = GameObject.Find("FirstPersonCharacter").transform.Find("ZoomInDisplay").gameObject.GetComponent<ZoomInDisplay>();
+        ChangeScene[] css = GetComponents<ChangeScene>();
+        foreach(ChangeScene scene in css)
+        {
+            if (scene.scene == "floor1") down = scene;
+            else up = scene;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (camera.enabled)
+        {
+            float v = Input.GetAxis("Vertical");
+            if (v > 0.1)
+            {
+                display.setCamera(null);
+                up.change();
+            }
+            else if(v < -0.1)
+            {
+                display.setCamera(null);
+                down.change();
+            }
+        }
+	}
+}
