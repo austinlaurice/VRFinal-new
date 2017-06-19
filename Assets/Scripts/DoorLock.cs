197fc0b49a78966057e6cd9bd26d9ace62c54e95
tenyoku8478 <sh961014@gmail.com>
2017-06-19 11:15:31 +0800

    first step checked

---
+++ Assets/Scripts/DoorLock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using System;
+
+public class DoorLock : MonoBehaviour, IPointerClickHandler, Condition {
+    public string keyName;
+    private bool unlocked;
+    public bool isTrue()
+    {
+        return unlocked;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(Pickable.itemName() == keyName)
+        {
+            Pickable.useItem();
+            unlocked = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        unlocked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
